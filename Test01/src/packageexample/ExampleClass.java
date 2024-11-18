631d2fe8d429725b775d866142b94aa5f6a302cd
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2024-11-18 15:50:32 +0100

    added example code

--- Test01/src/packageexample/ExampleClass.java
+++ Test01/src/packageexample/ExampleClass.java
@@ -4,7 +4,24 @@ public class ExampleClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("It works");
+		
+		int x = 8;
+		int y = 2000;
+		
+		int z = x * y;
+		
+		System.out.println("Multiplacation result is "+ z);
+		
+		if (x < y) {
+			System.out.println("x is smaller");
+		}
+		else {
+			System.out.println("y is smaller");
+		}
+		
+		for (int i = 0; i < x; i++) {
+			System.out.println(i * i);
+		}
 	}
 
 }
